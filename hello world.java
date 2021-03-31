7015b35b8ce0bce77b484fd416dec75f331d050a
HameedhulArshadh <38315040+HameedhulArshadh@users.noreply.github.com>
2021-03-31 12:05:39 +0530

    Add files via upload
    contains program that prints hello world

---
+++ hello world.java
@@ -0,0 +1,7 @@
+// to print hello world
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); //prints "Hello, World!" to the terminal
+    }
+}
